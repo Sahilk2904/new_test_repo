50259cef431f415cc4ac43d666cf38299c00e9a2
Sahilk2904 <sahil.k16635@gmail.com>
2025-09-02 01:21:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.41.0"
     }
   }
 }
 
 provider "azurerm" {
-    subscription_id = "fbac4321-0f41-46f0-baea-f23a62385eb5"
+  features {}
+  subscription_id = "fbac4321-0f41-46f0-baea-f23a62385eb5"
   # Configuration options
 }
 
-resource "azurerm_resource_group" "mithuastad"{
-    name = shanu-rg1
-  location = "central india"
+resource "azurerm_resource_group" "mithuastad" {
+  name     = "shanu-rg1"
+  location = "Central india"
 }
